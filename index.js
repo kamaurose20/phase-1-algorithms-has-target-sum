5f6af5945e00f4bfda1ed97f242f716a60cf6751
rosekamau <kamaurose236@gmail.com>
2024-07-21 15:14:20 +0300

    aLgorithms has target sum

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++) {
+      if  (array[i] + array[j] === target) {
+      return true; }
+     }
+  }
+  return false;
 }
 
 /* 
@@ -7,6 +14,11 @@ function hasTargetSum(array, target) {
 */
 
 /* 
+Iterate through array,
+add numbers in array until sum = target
+If sum = target , return true
+else, return false.
+
   Add your pseudocode here
 */
 
